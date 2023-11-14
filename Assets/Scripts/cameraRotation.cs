6ac5a556eab2823caacffe19c743d4d4cb0ff9df
Dean <111811264+DeanLemans@users.noreply.github.com>
2023-11-14 16:23:41 +0100

    incomplete camera

--- Assets/Scripts/cameraRotation.cs
+++ Assets/Scripts/cameraRotation.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class cameraRotation : MonoBehaviour
 {
+    public Transform player;
     private float xMouse;
     private float yMouse;
     private float xRotation;
+
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
-        
+        //xMouse = 
+        //yMouse = 
     }
 }
